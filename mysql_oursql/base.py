28d6964aa8179266ca899b86ad70c5478eb2e8c5
David Cramer <dcramer@David-Cramers-MacBook.local>
2009-11-30 14:52:54 -0600

    (tmp) monkey patch settings for geodjango. fix _replace_params to use sub not replace. remove converters. remove encoders (todo). Change get_server_info() to use server_info

--- mysql_oursql/base.py
+++ mysql_oursql/base.py
@@ -12,15 +12,13 @@ except ImportError, e:
     from django.core.exceptions import ImproperlyConfigured
     raise ImproperlyConfigured("Error loading oursql module: %s" % e)
 
-from oursql import _conversions as django_conversions
-
 from django.db.backends import *
 from django.db.backends.signals import connection_created
 from django.db.backends.mysql.client import DatabaseClient
 from django.db.backends.mysql.creation import DatabaseCreation
 from django.db.backends.mysql.introspection import DatabaseIntrospection
 from django.db.backends.mysql.validation import DatabaseValidation
-from django.utils.safestring import SafeString, SafeUnicode
+# from django.utils.safestring import SafeString, SafeUnicode
 
 # Raise exceptions for database warnings if DEBUG is on
 from django.conf import settings
@@ -31,34 +29,21 @@ if settings.DEBUG:
 DatabaseError = Database.DatabaseError
 IntegrityError = Database.IntegrityError
 
-# TODO: is this still valid for oursql?
-# MySQLdb-1.2.1 returns TIME columns as timedelta -- they are more like
-# timedelta in terms of actual behavior as they are signed and include days --
-# and Django expects time, so we still need to override that. We also need to
-# add special handling for SafeUnicode and SafeString as MySQLdb's type
-# checking is too tight to catch those (see Django ticket #6052).
-# django_conversions = conversions.copy()
-# django_conversions.update({
-#     FIELD_TYPE.TIME: util.typecast_time,
-#     FIELD_TYPE.DECIMAL: util.typecast_decimal,
-#     FIELD_TYPE.NEWDECIMAL: util.typecast_decimal,
-# })
-
 # This should match the numerical portion of the version numbers (we can treat
 # versions like 5.0.24 and 5.0.24a as the same). Based on the list of version
 # at http://dev.mysql.com/doc/refman/4.1/en/news.html and
 # http://dev.mysql.com/doc/refman/5.0/en/news.html .
 server_version_re = re.compile(r'(\d{1,2})\.(\d{1,2})\.(\d{1,2})')
 
-# TODO: is this still valid in oursql?
-# MySQLdb-1.2.1 and newer automatically makes use of SHOW WARNINGS on
-# MySQL-4.1 and newer, so the MysqlDebugWrapper is unnecessary. Since the
-# point is to raise Warnings as exceptions, this can be done with the Python
-# warning module, and this is setup when the connection is created, and the
-# standard util.CursorDebugWrapper can be used. Also, using sql_mode
-# TRADITIONAL will automatically cause most warnings to be treated as errors.
-
 params_re = re.compile(r'%[a-zA-Z]')
+
+# TODO: monkey patch django to support our package in areas such as GIS is not a good solution
+try:
+    from django.conf import settings
+    if settings.DATABASE_ENGINE.startswith('mysql'):
+        settings.DATABASE_ENGINE = 'mysql'
+except ImportError:
+    pass
 
 class CursorWrapper(object):
     """
@@ -74,7 +59,7 @@ class CursorWrapper(object):
         self.cursor = cursor
         
     def _replace_params(self, query):
-        return params_re.replace('?', query)
+        return params_re.sub('?', query)
 
     def execute(self, query, args=None):
         query = self._replace_params(query)
@@ -255,7 +240,6 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     def _cursor(self):
         if not self._valid_connection():
             kwargs = {
-                'conv': django_conversions,
                 'charset': 'utf8',
                 'use_unicode': True,
             }
@@ -275,10 +259,12 @@ class DatabaseWrapper(BaseDatabaseWrapper):
             # We need the number of potentially affected rows after an
             # "UPDATE", not the number of changed rows.
             kwargs['found_rows'] = True
+            # TODO: support for 'init_command'
             kwargs.update(settings_dict['DATABASE_OPTIONS'])
             self.connection = Database.connect(**kwargs)
-            self.connection.encoders[SafeUnicode] = self.connection.encoders[unicode]
-            self.connection.encoders[SafeString] = self.connection.encoders[str]
+            # XXX: oursql does not have encoders like mysqldb -- unknown if this is still needed
+            # self.connection.encoders[SafeUnicode] = self.connection.encoders[unicode]
+            # self.connection.encoders[SafeString] = self.connection.encoders[str]
             connection_created.send(sender=self.__class__)
         cursor = CursorWrapper(self.connection.cursor())
         return cursor
@@ -293,8 +279,8 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         if not self.server_version:
             if not self._valid_connection():
                 self.cursor()
-            m = server_version_re.match(self.connection.get_server_info())
+            m = server_version_re.match(self.connection.server_info)
             if not m:
-                raise Exception('Unable to determine MySQL version from version string %r' % self.connection.get_server_info())
+                raise Exception('Unable to determine MySQL version from version string %r' % self.connection.server_info)
             self.server_version = tuple([int(x) for x in m.groups()])
         return self.server_version
