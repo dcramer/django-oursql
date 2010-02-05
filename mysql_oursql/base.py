f7aabd01bf7ce584b7e72b81c78665658774dbe6
David Cramer <dcramer@David-Cramers-MacBook.local>
2010-02-05 11:30:44 -0600

    Revert "hard code FIELD_TYPES"
    This reverts commit 339a5b3ec9103ec823ac71bb43e3519f88a0c9f8.

--- mysql_oursql/base.py
+++ mysql_oursql/base.py
@@ -225,7 +225,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         self.client = DatabaseClient(self)
         self.creation = DatabaseCreation(self)
         self.introspection = DatabaseIntrospection(self)
-        self.validation = DatabaseValidation(self.connection)
+        self.validation = DatabaseValidation()
 
     def _valid_connection(self):
         if self.connection is not None:
